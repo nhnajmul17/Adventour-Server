a739e41a6857c09b5f633e94f1950b2ca3b40852
nhnajmul17 <nhnajmul17@gmail.com>
2021-10-30 23:03:22 +0600

    update booking

--- index.js
+++ index.js
@@ -53,6 +53,14 @@ async function run() {
             res.json(result)
         })
 
+        //get A single Booking
+        app.get('/bookings/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await bookingCollection.findOne(query)
+            res.send(result)
+        })
+
         //get all bookings
         app.get('/bookings', async (req, res) => {
             const cursor = bookingCollection.find({})
